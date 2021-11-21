7140fb61c386d5719101c1dea1001e12d8fd22dc
Dylan Alvarez <dylan.alvarez@greencodesoftware.com>
2021-11-21 06:05:13 -0300

    Add point c to FilterRows

--- src/point_c/answer_column_dropper_c/main.py
+++ src/point_c/answer_column_dropper_c/main.py
@@ -1,31 +1,19 @@
 #!/usr/bin/env python3
 import os
-import json
 
-from common.connect_to_rabbitmq import connect_to_rabbitmq
-from common.send_end import send_end
+from common.middleware.filter_rows import FilterRows
 from common.get_node_id import get_node_id
 
-with connect_to_rabbitmq() as channel:
-    joiner_by_question_id_count = int(os.getenv('JOINER_BY_QUESTION_ID_COUNT'))
-    channel.queue_declare(queue='point_c_answers')
-    for node_id in range(joiner_by_question_id_count):
-        channel.queue_declare(queue=f'answer_id_with_year_score_{node_id}')
+joiner_by_question_id_count = int(os.getenv('JOINER_BY_QUESTION_ID_COUNT'))
 
-    def handle_answer(_ch, method, _properties, body):
-        body = body.decode("ISO-8859-1")
-        if body == '__end__':
-            for node_id in range(joiner_by_question_id_count):
-                send_end(channel, exchange='', routing_key=f'answer_id_with_year_score_{node_id}')
-            channel.basic_ack(delivery_tag=method.delivery_tag)
-            exit(0)
-        else:
-            answer = json.loads(body)
-            channel.basic_publish(
-                exchange='',
-                routing_key=f'answer_id_with_year_score_{str(get_node_id(answer["question_id"], joiner_by_question_id_count))}',
-                body=' '.join((answer["question_id"], answer["creation_date"][0:4], answer["score"]))
-            )
+end_message_count_by_output_queue = {}
+for node_id in range(joiner_by_question_id_count):
+    end_message_count_by_output_queue[f'answer_id_with_year_score_{node_id}'] = 1
 
-    channel.basic_consume(queue='point_c_answers', on_message_callback=handle_answer)
-    channel.start_consuming()
+FilterRows(
+    input_queue='point_c_answers',
+    map_row_to_message_by_output_queue=lambda answer: {
+        f'answer_id_with_year_score_{str(get_node_id(answer["question_id"], joiner_by_question_id_count))}': ' '.join((answer["question_id"], answer["creation_date"][0:4], answer["score"]))
+    },
+    end_message_count_by_output_queue=end_message_count_by_output_queue
+).run()
--- src/point_c/question_column_dropper_c/main.py
+++ src/point_c/question_column_dropper_c/main.py
@@ -1,31 +1,19 @@
 #!/usr/bin/env python3
 import os
-import json
 
-from common.connect_to_rabbitmq import connect_to_rabbitmq
-from common.send_end import send_end
+from common.middleware.filter_rows import FilterRows
 from common.get_node_id import get_node_id
 
-with connect_to_rabbitmq() as channel:
-    joiner_by_question_id_count = int(os.getenv('JOINER_BY_QUESTION_ID_COUNT'))
-    channel.queue_declare(queue='point_c_questions')
-    for node_id in range(joiner_by_question_id_count):
-        channel.queue_declare(queue=f'question_id_with_year_score_{node_id}')
+joiner_by_question_id_count = int(os.getenv('JOINER_BY_QUESTION_ID_COUNT'))
 
-    def handle_question(_ch, method, _properties, body):
-        body = body.decode("ISO-8859-1")
-        if body == '__end__':
-            for node_id in range(joiner_by_question_id_count):
-                send_end(channel, exchange='', routing_key=f'question_id_with_year_score_{node_id}')
-            channel.basic_ack(delivery_tag=method.delivery_tag)
-            exit(0)
-        else:
-            question = json.loads(body)
-            channel.basic_publish(
-                exchange='',
-                routing_key=f'question_id_with_year_score_{str(get_node_id(question["question_id"], joiner_by_question_id_count))}',
-                body=' '.join((question["question_id"], question["creation_date"][0:4], question["score"], question["tags"]))
-            )
+end_message_count_by_output_queue = {}
+for node_id in range(joiner_by_question_id_count):
+    end_message_count_by_output_queue[f'question_id_with_year_score_{node_id}'] = 1
 
-    channel.basic_consume(queue='point_c_questions', on_message_callback=handle_question)
-    channel.start_consuming()
+FilterRows(
+    input_queue='point_c_questions',
+    map_row_to_message_by_output_queue=lambda question: {
+        f'question_id_with_year_score_{str(get_node_id(question["question_id"], joiner_by_question_id_count))}': ' '.join((question["question_id"], question["creation_date"][0:4], question["score"], question["tags"]))
+    },
+    end_message_count_by_output_queue=end_message_count_by_output_queue
+).run()
